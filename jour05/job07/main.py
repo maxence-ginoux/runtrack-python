f61b167951f77bb7f95224cdd2716ac623feaff2
maxence.ginoux <maxence.ginoux@laplateforme.io>
2023-11-30 10:29:01 +0100

    suite

--- jour05/job07/main.py
+++ jour05/job07/main.py
@@ -1,1 +1,18 @@
-def notes_eleves():
+def evaluation_eleve():
+    note = float(input("Entrez votre note sur 100 : "))
+
+    if note >= 40:
+        multiple_de_5 = ((note // 5) + 1) * 5
+        difference = multiple_de_5 - note
+
+        if difference < 3:
+            note_arrondie = multiple_de_5
+        else:
+            note_arrondie = note
+
+        print("Félicitations, vous avez réussi le test!")
+        print("La note est arrondie à", note_arrondie)
+    else:
+        print("Désolé, vous avez échoué au test.")
+
+evaluation_eleve()
